df1b9a1726bf85ccd36f4f4df567ff09ce786d8e
davidlion <david.lion@yscope.com>
2025-10-19 14:17:39 -0400

    Add self. to LogEvent again...

--- src/log_surgeon/log_event.py
+++ src/log_surgeon/log_event.py
@@ -6,13 +6,13 @@ class LogEvent:
         self._var_dict: dict[str, str | list[str | int | float]] = {}
 
     def get_log_message(self) -> str:
-        return _log_message
+        return self._log_message
 
     def __getitem__(self, variable_name: str) -> str | list[str | int | float]:
-        return _var_dict[variable_name]
+        return self._var_dict[variable_name]
 
     def __str__(self) -> str:
-        return get_log_message()
+        return self.get_log_message()
 
     def __repr__(self) -> str:
-        return get_log_message()
+        return self.get_log_message()
